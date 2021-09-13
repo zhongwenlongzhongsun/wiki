7ef62dc8ec6d4b67ccbb7e57731bc16eb196401c
zhongwenlongzhongsun <361651398@qq.com>
2021-09-13 18:10:35 +0800

    3-4 mybatis示例：接口+表数据

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @RestController //返回字符串
@@ -12,6 +17,9 @@ public class TestController {
 
     @Value("${test.hello}") //扫描此配置项
     private String testHello;
+
+    @Resource
+    private TestService testService;
 //    @GetMapping
 //    @PutMapping
 //    @DeleteMapping
@@ -25,5 +33,10 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "Hello World Post, " + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
---
+++ src/main/java/com/jiawa/wiki/service/TestService.java
@@ -0,0 +1,20 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+//    @Autowired
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
