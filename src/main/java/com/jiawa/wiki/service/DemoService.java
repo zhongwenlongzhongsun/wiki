61c710661dbd35e6e62eab675a1c155fa96305d4
zhongwenlongzhongsun <361651398@qq.com>
2021-09-13 21:17:12 +0800

    3-5 增加demo表列表查询

---
+++ src/main/java/com/jiawa/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Demo;
+import com.jiawa.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+//    @Autowired
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
