b91160a9affd686506b6ae44738416400ed66753
zhongwenlongzhongsun <361651398@qq.com>
2021-09-13 11:59:43 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 // @Controller  // 返回页面
 public class TestController {
 
+    @Value("${test.hello}") //扫描此配置项
+    private String testHello;
 //    @GetMapping
 //    @PutMapping
 //    @DeleteMapping
@@ -16,7 +19,7 @@ public class TestController {
 //    @RequestMapping("/hello")
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World" + testHello;
     }
 
     @PostMapping("/hello/post")
