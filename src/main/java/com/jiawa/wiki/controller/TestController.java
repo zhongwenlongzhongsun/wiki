3062cfd194b9cf459859ea540ac59f47fd510821
zhongwenlongzhongsun <361651398@qq.com>
2021-09-13 11:23:02 +0800

    2-6 Hello World 接口测试http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController //返回字符串
 // @Controller  // 返回页面
@@ -15,5 +17,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World Post, " + name;
     }
 }
