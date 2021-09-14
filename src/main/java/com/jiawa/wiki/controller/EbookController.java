4ae0b9cecfe490f931df419d5cb9d18b0cbf2a2d
zhongwenlongzhongsun <361651398@qq.com>
2021-09-14 10:10:29 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,25 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+
+@RestController //返回字符串
+// @Controller  // 返回页面
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -0,0 +1,20 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+//    @Autowired
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
