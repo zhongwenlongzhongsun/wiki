b1f4e131103ff622ffe7fe3de58013021470fe79
zhongwenlongzhongsun <361651398@qq.com>
2021-09-14 13:15:00 +0800

    3-8 制作CopyUtil封装BeanUtils

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.jiawa.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,15 +26,21 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-//            ebookResp.setId(ebook.getId());
-            //Spring自带的工具类从 ebook 拷贝到 ebookResp,不需要一个一个的 setId(ebook.getId()); ....
-            BeanUtils.copyProperties(ebook, ebookResp);
-            //现在ebookResp有值了
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+//////            ebookResp.setId(ebook.getId());
+////            //Spring自带的工具类从 ebook 拷贝到 ebookResp,不需要一个一个的 setId(ebook.getId()); ....
+////            BeanUtils.copyProperties(ebook, ebookResp);
+
+//            // 对象复制                           源    -> 目标类
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            //现在ebookResp有值了
+//            respList.add(ebookResp);
+//        }
+
+        //列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
