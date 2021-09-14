78ae86f9ba92d7c72ae60b7fb49339d1c3f5e136
zhongwenlongzhongsun <361651398@qq.com>
2021-09-14 18:09:41 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header></the-header>
       <router-view/>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2021 Created by ZhongSun
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ant Design ©2021 Created by ZhongSun
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+    name: 'the-footer'
+});
+</script>
