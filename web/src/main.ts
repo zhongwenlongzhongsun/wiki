da379fef49e0f17e80c87a7933d882359e5902a3
zhongwenlongzhongsun <361651398@qq.com>
2021-09-15 11:15:58 +0800

    5-5 Vue多环境配置，增加dev和prod配置文件     编译和启动命令增加多环境

--- web/src/main.ts
+++ web/src/main.ts
@@ -15,3 +15,7 @@ const icons : any = Icons;
 for(const i in icons) {
     app.component(i, icons[i]);
 }
+
+//使用process.env.xxx 读环境配置
+console.log('环境：', process.env.NODE_ENV);
+console.log('服务端：', process.env.VUE_APP_SERVER);
