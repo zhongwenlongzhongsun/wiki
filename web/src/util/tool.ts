10a488ef0741ec1b0c3f3a86ede9307d467efdd0
zhongwenlongzhongsun <361651398@qq.com>
2022-05-18 15:38:29 +0800

    7-4 分类表格按树形结构展示

--- web/src/util/tool.ts
+++ web/src/util/tool.ts
@@ -2,7 +2,7 @@ export class Tool {
   /**
    * 空校验 null或""都返回true
    */
-  public static isEmpty (obj: any) {
+  public static isEmpty(obj: any) {
     if ((typeof obj === 'string')) {
       return !obj || obj.replace(/\s+/g, "") === ""
     } else {
@@ -13,7 +13,7 @@ export class Tool {
   /**
    * 非空校验
    */
-  public static isNotEmpty (obj: any) {
+  public static isNotEmpty(obj: any) {
     return !this.isEmpty(obj);
   }
 
@@ -21,7 +21,7 @@ export class Tool {
    * 对象复制
    * @param obj
    */
-  public static copy (obj: object) {
+  public static copy(obj: object) {
     if (Tool.isNotEmpty(obj)) {
       return JSON.parse(JSON.stringify(obj));
     }
@@ -31,7 +31,7 @@ export class Tool {
    * 使用递归将数组转为树形结构
    * 父ID属性为parent
    */
-  public static array2Tree (array: any, parentId: number) {
+  public static array2Tree(array: any, parentId: number) {
     if (Tool.isEmpty(array)) {
       return [];
     }
@@ -51,23 +51,5 @@ export class Tool {
       }
     }
     return result;
-  }
-
-  /**
-   * 随机生成[len]长度的[radix]进制数
-   * @param len
-   * @param radix 默认62
-   * @returns {string}
-   */
-  public static uuid (len: number, radix = 62) {
-    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-    const uuid = [];
-    radix = radix || chars.length;
-
-    for (let i = 0; i < len; i++) {
-      uuid[i] = chars[0 | Math.random() * radix];
-    }
-
-    return uuid.join('');
   }
 }
--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -1,15 +1,10 @@
 <template>
   <a-layout>
     <a-layout-content
-        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '540px' }"
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <p>
         <a-form layout="inline" :model="param">
-          <a-form-item>
-            <a-input v-model:value="param.name" placeholder="   名称">
-              <template #prefix><QuestionCircleOutlined style="color: rgba(0, 0, 0, 0.25)"/></template>
-            </a-input>
-          </a-form-item>
           <a-form-item>
             <a-button type="primary" @click="handleQuery()">
               查询
@@ -22,19 +17,31 @@
           </a-form-item>
         </a-form>
       </p>
+      <p>
+        <a-alert
+            class="tip"
+            message="小提示：这里的分类会显示到首页的侧边菜单"
+            type="info"
+            closable
+        />
+      </p>
       <a-table
+          v-if="level1.length > 0"
           :columns="columns"
           :row-key="record => record.id"
           :data-source="level1"
           :loading="loading"
           :pagination="false"
+          :defaultExpandAllRows="true"
       >
-        <template #cover="{text: cover }">
-          <img v-if="cover" :src="cover" alt="avatar" style="width: 60px; height: 60px"/>
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar" />
         </template>
-        <template v-slot:action="{ text, record}">
+        <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary" @click="edit(record)">编辑</a-button>
+            <a-button type="primary" @click="edit(record)">
+              编辑
+            </a-button>
             <a-popconfirm
                 title="删除后不可恢复，确认删除?"
                 ok-text="是"
@@ -55,24 +62,31 @@
       title="分类表单"
       v-model:visible="modalVisible"
       :confirm-loading="modalLoading"
-      @ok="handleModalOK"
+      @ok="handleModalOk"
   >
-    <a-form :model="category" :label-col="{ span: 6}" :wrapper-col="{span: 18}">
-
+    <a-form :model="category" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
       <a-form-item label="名称">
         <a-input v-model:value="category.name" />
       </a-form-item>
-      <a-form-item label="副分类">
-        <a-input v-model:value="category.parent" />
+      <a-form-item label="父分类">
+        <a-select
+            v-model:value="category.parent"
+            ref="select"
+        >
+          <a-select-option :value="0">
+            无
+          </a-select-option>
+          <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id === c.id">
+            {{c.name}}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="category.sort" />
       </a-form-item>
-
     </a-form>
   </a-modal>
 </template>
-
 
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue';
@@ -82,11 +96,10 @@ import {Tool} from "@/util/tool";
 
 export default defineComponent({
   name: 'AdminCategory',
-  //Vue3 新增的初始化方法
   setup() {
     const param = ref();
     param.value = {};
-    const categorys = ref();//响应式数据
+    const categorys = ref();
     const loading = ref(false);
 
     const columns = [
@@ -94,11 +107,11 @@ export default defineComponent({
         title: '名称',
         dataIndex: 'name'
       },
-      {
-        title: '父分类',
-        key: 'parent',
-        dataIndex: 'parent'
-      },
+      // {
+      //   title: '父分类',
+      //   key: 'parent',
+      //   dataIndex: 'parent'
+      // },
       {
         title: '顺序',
         dataIndex: 'sort'
@@ -107,94 +120,102 @@ export default defineComponent({
         title: 'Action',
         key: 'action',
         slots: { customRender: 'action' }
-      },
+      }
     ];
+
     /**
-     * 一级分类树，children属性是二级分类
-    [{
-      id:"",
-      name:"",
-      children:[{
-        id:"",
-        name:"",
-      }]
-    }]
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
      */
-    const level1 = ref(); // 一级分类树，children属性是二级分类
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    level1.value = [];
+
     /**
      * 数据查询
-     */
-    const handleQuery = () =>{
+     **/
+    const handleQuery = () => {
       loading.value = true;
-      axios.get("/category/all").then((response) =>{
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      level1.value = [];
+      axios.get("/category/all").then((response) => {
         loading.value = false;
         const data = response.data;
-        if (data.success){
+        if (data.success) {
           categorys.value = data.content;
-          console.log("原始数组:", categorys.value);
+          console.log("原始数组：", categorys.value);
+
           level1.value = [];
-          level1.value = Tool.array2Tree(category.value,0);
-          console.log("树形结构",level1);
-        }else{
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构：", level1);
+        } else {
           message.error(data.message);
         }
       });
     };
 
-    // --表单--
+    // -------- 表单 ---------
     const category = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
-    const handleModalOK = () =>{
+    const handleModalOk = () => {
       modalLoading.value = true;
-      axios.post("/category/save", category.value).then((response) =>{
+      axios.post("/category/save", category.value).then((response) => {
         modalLoading.value = false;
-        const data = response.data;  // data = commonResp
-        if (data.success){
+        const data = response.data; // data = commonResp
+        if (data.success) {
           modalVisible.value = false;
 
-          //重新加载列表
-          handleQuery(); // 只在方法内部调用不用return
-        }else{
+          // 重新加载列表
+          handleQuery();
+        } else {
           message.error(data.message);
         }
       });
     };
 
-    //编辑
-    const edit = (record : any) => {
+    /**
+     * 编辑
+     */
+    const edit = (record: any) => {
       modalVisible.value = true;
       category.value = Tool.copy(record);
-    }
+    };
 
-    //新增
+    /**
+     * 新增
+     */
     const add = () => {
       modalVisible.value = true;
       category.value = {};
-    }
+    };
 
-    //删除(long类型对应的前端类型是number)
-    const handleDelete = (id : number) =>{
-      axios.delete("/category/delete/" + id).then((response) =>{
-        const data = response.data;  // data = commonResp
-        if (data.success){
-          modalVisible.value = false;
-          modalLoading.value = false;
-          //重新加载列表
-          handleQuery(); // 只在方法内部调用不用return
+    const handleDelete = (id: number) => {
+      axios.delete("/category/delete/" + id).then((response) => {
+        const data = response.data; // data = commonResp
+        if (data.success) {
+          // 重新加载列表
+          handleQuery();
+        } else {
+          message.error(data.message);
         }
       });
     };
 
     onMounted(() => {
-      handleQuery(); // 只在方法内部调用不用return
+      handleQuery();
     });
-
 
     return {
       param,
+      // categorys,
       level1,
-      categorys,
       columns,
       loading,
       handleQuery,
@@ -205,10 +226,17 @@ export default defineComponent({
       category,
       modalVisible,
       modalLoading,
-      handleModalOK,
-      handleDelete,
+      handleModalOk,
 
+      handleDelete
     }
   }
 });
 </script>
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
