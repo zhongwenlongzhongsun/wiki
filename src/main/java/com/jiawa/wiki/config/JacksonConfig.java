41e3f4c3793e0c7acb6ca339c56e1d3d0d40fecc
zhongwenlongzhongsun <361651398@qq.com>
2022-09-05 17:20:35 +0800

    9-12 前后端增加登录拦截

--- src/main/java/com/jiawa/wiki/config/JacksonConfig.java
+++ src/main/java/com/jiawa/wiki/config/JacksonConfig.java
@@ -1,28 +1,28 @@
-package com.jiawa.wiki.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-@Configuration
-public class JacksonConfig {  //处理接口返回id与数据库id不一致问题 (雪花算法前后端交互Long类型精度丢失的问题)
-    @Bean
-    @Primary
-    @ConditionalOnMissingBean(ObjectMapper.class)
-    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
-    {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-
-        // 全局配置序列化返回 JSON 处理
-        SimpleModule simpleModule = new SimpleModule();
-        //JSON Long ==> String
-        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-        objectMapper.registerModule(simpleModule);
-        return objectMapper;
-    }
-}
+//package com.jiawa.wiki.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.module.SimpleModule;
+//import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+//
+//@Configuration
+//public class JacksonConfig {  //处理接口返回id与数据库id不一致问题 (雪花算法前后端交互Long类型精度丢失的问题)
+//    @Bean
+//    @Primary
+//    @ConditionalOnMissingBean(ObjectMapper.class)
+//    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder)
+//    {
+//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+//
+//        // 全局配置序列化返回 JSON 处理
+//        SimpleModule simpleModule = new SimpleModule();
+//        //JSON Long ==> String
+//        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+//        objectMapper.registerModule(simpleModule);
+//        return objectMapper;
+//    }
+//}
--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -1,7 +1,16 @@
 package com.jiawa.wiki.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.jiawa.wiki.interceptor.LogInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,11 +28,40 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")
                 .excludePathPatterns( //无需拦截（无登录）
                         "/test/**",
+                        "/redis/**",
                         "/user/login",
                         "/category/all",
                         "/ebook/list",
                         "/doc/all/**",
                         "/doc/find-content/**"
                 );
+    }
+
+    /**
+     * 解决跨域类
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //地址的请求地址
+                .allowedOriginPatterns("*") //允许来源
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true) //凭证
+                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+    }
+
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+
+        // 全局配置序列化返回 JSON 处理
+        SimpleModule simpleModule = new SimpleModule();
+        //JSON Long ==> String
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
     }
 }
